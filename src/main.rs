43d646b34572517f2c2e292e132e2ed3c703cbb2
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-11-03 22:25:10 +0000

    Implement deletion variant generation code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::io;
 use std::io::{Read, Write};
 
@@ -25,10 +26,105 @@ fn nearust(
     // The error stream is currently unused as we do not handle any errors, and thus its name is
     // prefixed with an underscore.
 
+    let strings = get_string_vector(in_stream).unwrap();
+
+    // Make hash map of all possible substrings that can be generated from input strings via making
+    // deletions up to the threshold level, where the keys are the substrings and the values are
+    // vectors of indices corresponding to the input strings from which the substrings can be
+    // generated.
+    let mut variant_dict: HashMap<String, Vec<usize>> = HashMap::new();
+    for (idx, s) in strings.iter().enumerate() {
+        let variants = get_deletion_variants(s, 1).unwrap();
+        for v in variants.iter() {
+            let entry = variant_dict.entry(v.to_string()).or_default();
+            entry.push(idx);
+        }
+    }
+
+    // TODO: iterate through the hashmap generated above, examine and double check hits to see if
+    // they are real (this will require an implementation of Levenshtein distance)
+    // TODO: Make a record of all validated hits, and print them out to out_stream
+
+    let _ = out_stream.write("1,2".as_bytes()).unwrap();
+    Ok(())
+}
+
+fn get_string_vector(in_stream: &mut impl Read) -> Result<Vec<String>, io::Error> {
+    // Read lines from in_stream until EOF and collect into vector of string slices.
+
     let mut in_buffer = "".to_string();
     in_stream.read_to_string(&mut in_buffer)?;
-    let _ = out_stream.write("1,2".as_bytes())?;
-    Ok(())
+    let strings: Vec<String> = in_buffer.lines().map(|line| line.to_string()).collect();
+    Ok(strings)
+}
+
+fn get_deletion_variants(input: &str, max_deletions: u8) -> Result<HashSet<String>, &'static str> {
+    // Given an input string, generate all possible strings after making at most max_deletions
+    // single-character deletions.
+
+    if max_deletions > 2 {
+        return Err("Thresholds above 2 edit distance are unsupported.")
+    }
+
+    let input_length = input.chars().count();
+    if input_length > 255 {
+        return Err("Input strings longer than 255 characters are unsupported.")
+    }
+
+    let mut deletion_variants = HashSet::new();
+    deletion_variants.insert(input.to_string());
+    
+    for num_deletions in 1..=max_deletions {
+        if num_deletions > input_length as u8 {
+            deletion_variants.insert("".to_string());
+            continue
+        }
+
+        for deletion_indices in get_k_combinations(input_length, num_deletions as usize)? {
+            let mut variant = "".to_string();
+            let mut offset = 0;
+
+            for idx in deletion_indices.iter() {
+                variant += &input[offset..*idx];
+                offset = idx + 1;
+            }
+            variant += &input[offset..input_length];
+
+            deletion_variants.insert(variant);
+        }
+    }
+
+    Ok(deletion_variants)
+}
+
+fn get_k_combinations(n: usize, k: usize) -> Result<Vec<Vec<usize>>, &'static str> {
+    // Return a vector containing all k-combinations of the integers in the range 0..n.
+
+    if k > n {
+        return Err("k cannot be larger than n")
+    }
+
+    let mut combinations: Vec<Vec<usize>> = Vec::new();
+    let mut current_combination: Vec<usize> = Vec::new();
+
+    combination_search(n, k, 0, &mut current_combination, &mut combinations);
+
+    Ok(combinations)
+}
+
+fn combination_search(n: usize, k: usize, start: usize, current_combination: &mut Vec<usize>, combinations: &mut Vec<Vec<usize>>) {
+    // Recursive function used in computing k-combinations.
+
+    if current_combination.len() == k {
+        combinations.push(current_combination.clone());
+        return
+    };
+
+    for idx in start..n {
+        current_combination.push(idx);
+        combination_search(n, k, idx+1, current_combination, combinations);
+        current_combination.pop();
+    };
 }
 
 #[cfg(test)]
@@ -36,13 +132,50 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sanity_test() {
-        // This below is not meant to be a real test of code functionality, but is just here to
-        // give us somewhere to start, and to make sure the testing boilerplate is ready.
-
+    fn test_nearust() {
         let mut out = Vec::new();
         let mut err = Vec::new();
         nearust(&mut "foo\nbar\nbaz".as_bytes(), &mut out, &mut err).unwrap();
         assert_eq!(out, b"1,2");
+    }
+
+    #[test]
+    fn test_get_string_vector() {
+        let strings = get_string_vector(&mut "foo\nbar\nbaz\n".as_bytes()).unwrap();
+        let expected = vec!["foo", "bar", "baz"];
+        assert_eq!(strings, expected);
+    }
+
+    #[test]
+    fn test_get_k_combinations() {
+        let combinations = get_k_combinations(3, 2).unwrap();
+        let expected = vec![
+            vec![0,1],
+            vec![0,2],
+            vec![1,2]
+        ];
+        assert_eq!(combinations, expected);
+
+        let error = get_k_combinations(2, 3);
+        assert!(matches!(error, Err(_)))
+    }
+
+    #[test]
+    fn test_get_deletion_variants() {
+        let variants = get_deletion_variants("foo", 1).unwrap();
+        let mut expected = HashSet::new();
+        expected.insert("foo".to_string());
+        expected.insert("fo".to_string());
+        expected.insert("oo".to_string());
+        assert_eq!(variants, expected);
+
+        let variants = get_deletion_variants("foo", 2).unwrap();
+        let mut expected = HashSet::new();
+        expected.insert("foo".to_string());
+        expected.insert("fo".to_string());
+        expected.insert("oo".to_string());
+        expected.insert("f".to_string());
+        expected.insert("o".to_string());
+        assert_eq!(variants, expected);
     }
 }
