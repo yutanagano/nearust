7d2cc10cc67604d554110cb46cf89ebba5f2ebed
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-10-17 21:24:56 +0100

    fix: set zero-indexed to true by default in Python

--- rust/pymod.rs
+++ rust/pymod.rs
@@ -2,7 +2,7 @@ use super::*;
 use pyo3::prelude::*;
 
 #[pyfunction]
-#[pyo3(signature = (strings, max_edits = 2, zero_indexed = false))]
+#[pyo3(signature = (strings, max_edits = 2, zero_indexed = true))]
 fn symdel_within_set(
     strings: Vec<String>,
     max_edits: usize,
@@ -12,7 +12,7 @@ fn symdel_within_set(
 }
 
 #[pyfunction]
-#[pyo3(signature = (strings_primary, strings_comparison, max_edits = 2, zero_indexed = false))]
+#[pyo3(signature = (strings_primary, strings_comparison, max_edits = 2, zero_indexed = true))]
 fn symdel_across_sets(
     strings_primary: Vec<String>,
     strings_comparison: Vec<String>,
@@ -27,7 +27,6 @@ fn symdel_across_sets(
     ))
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn _lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(symdel_within_set, m)?)?;
