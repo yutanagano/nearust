bfefcf927471d4fbf4422cfdec693a8d2dcd4976
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-10-18 23:43:23 +0100

    feat: add docstring to symdel function

--- python/nearust/__init__.py
+++ python/nearust/__init__.py
@@ -8,6 +8,30 @@ __all__ = ["symdel"]
 def symdel(
     query: Iterable[str], reference: Iterable[str] = None, max_distance: int = 1
 ):
+    """
+    Fast, multi-threaded implementation of Chotisorayuth and Mayer's symdel
+    algorithm.
+
+    Parameters
+    ----------
+    query : iterable of str
+    reference : iterable of str, optional
+    max_distance : int, default=1
+        The maximum edit distance at which strings are considered neighbours.
+
+    Returns
+    -------
+    list of tuple of int, int, int
+        A list of integer triplets, where each triplet describes a pair of
+        strings that were detected as being neighbours, where the first two
+        ints describe the indices of the two strings in the pair, and the third
+        int describes the Levenshtein edit distance between them. If symdel was
+        only given `query` as input, then it returns pairs of strings from
+        within `query` that are neighbours. If symdel was given both `query`
+        and `reference`, then it returns string pairs across the two sets,
+        where the first index always corresponds to a string from `query`, and
+        the second index always corresponds to a string from `reference`.
+    """
     if reference is not None:
         return rustlib.symdel_across_sets(query, reference, max_distance, True)
 
