c174f0fc24f9f3f6e3d1f23959a2ed3eb77bf0ca
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-10-26 20:12:39 +0000

    chore(docstrings): document new CachedSymdel class

--- python/nearust/_symdel.py
+++ python/nearust/_symdel.py
@@ -9,8 +9,7 @@ def symdel(
     zero_index: bool = True,
 ) -> list[tuple[int, int, int]]:
     """
-    Fast, multi-threaded implementation of Chotisorayuth and Mayer's symdel
-    algorithm.
+    Quickly detects pairs of similar strings.
 
     Parameters
     ----------
@@ -34,11 +33,6 @@ def symdel(
         and `reference`, then it returns string pairs across the two sets,
         where the first index always corresponds to a string from `query`, and
         the second index always corresponds to a string from `reference`.
-
-    Raises
-    ------
-    ValueError
-        If any of the input strings contain non-ASCII characters.
 
     Examples
     --------
@@ -74,6 +68,24 @@ def symdel(
 
 
 class CachedSymdel:
+    """
+    A memoized implementation of symdel.
+
+    An implementation of symdel where the deletion variant computations for the
+    reference string set is memoized and stored in memory as a hashmap. This is
+    useful for use-cases where you want to repeatedly query the same reference,
+    especially if the reference is very large.
+
+    Parameters
+    ----------
+    reference : iterable of str
+    max_distance : int, default=1
+        The maximum edit distance that this CachedSymdel instance will be able
+        to support when detecting similar strings. That is, if `max_distance`
+        is set to X at construction, later calls to this instance's symdel
+        method will only be accept `max_distance` less than or equal to X.
+    """
+
     def __init__(self, reference: Iterable[str], max_distance: int = 1) -> None:
         self._internal = rustlib.CachedSymdel(reference, max_distance)
 
@@ -83,6 +95,72 @@ class CachedSymdel:
         max_distance: int = 1,
         zero_index: bool = True,
     ) -> list[tuple[int, int, int]]:
+        """
+        Quickly detects pairs of similar strings.
+
+        Parameters
+        ----------
+        query : iterable of str or CachedSymdel or None, default=None
+        max_distance : int, default=1
+            The maximum edit distance at which strings are considered
+            neighbours. This must be less than or equal to the `max_distance`
+            specified when constructing the caller instance (as well as that
+            when constructing `query`, if `query` is set to a CachedSymdel
+            instance).
+        zero_index : bool, default=True
+            If set to True, reports the indices of strings of interest using
+            0-based indexing. Otherwise uses 1-based indexing.
+
+        Returns
+        -------
+        list of tuple of int, int, int
+            A list of integer triplets, where each triplet describes a pair of
+            strings that were detected as being neighbours. The first two ints
+            describe the indices of the two strings in the pair, and the third
+            int describes the Levenshtein edit distance between them.
+
+            If `query` is not set (or set to None), the function computes
+            symdel for pairs of strings within `reference` (specified when
+            constructing the class). If `query` is set, the function computes
+            symdel for pairs of strings between the query and reference. In
+            this case, the first index in the triplet always corresponds to a
+            string from `query`, and the second index always corresponds to a
+            string from `reference`.
+
+            Note that if `query` is set to another CachedSymdel instance
+            constructed using some set of strings X, then the resulting
+            computation is equivalent to setting `query` to X directly. This is
+            useful in cases where you want to memoize deletion variant
+            computations for both query and reference sets.
+
+        Examples
+        --------
+        Construct a CachedSymdel instance with an iterable over reference
+        strings. This pre-computes the deletion variats for the strings in the
+        reference and stores the results in a hashmap held internally by the
+        instance.
+
+        >>> cached = nearust.CachedSymdel(["fooo", "barr", "bazz", "buzz"])
+
+        Then, call the symdel method with `query` set to an iterable over query
+        strings to find similar strings across it and the reference set.
+
+        >>> cached.symdel(["fizz", "fuzz", "buzz"])
+        [(1, 3, 1), (2, 2, 1), (2, 3, 0)]
+
+        If you also want to memoize deletion variant computations on the query
+        set as well, you can do so.
+
+        >>> cached_query = nearust.CachedSymdel(["fizz", "fuzz", "buzz"])
+        >>> cached.symdel(cached_query)
+        [(1, 3, 1), (2, 2, 1), (2, 3, 0)]
+
+        If you call symdel without specifying `query`, the function will look
+        for pairs of similar strings within the reference set.
+
+        >>> cached.symdel()
+        [(2, 3, 1)]
+        """
         if query is None:
             return self._internal.symdel_within(max_distance, zero_index)
 
