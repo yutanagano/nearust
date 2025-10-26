137e7b8e3497f91431821b7cc16c881b7abfccd3
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-10-26 21:00:42 +0000

    chore(docs): add error case to CachedSymdel docs

--- python/nearust/_symdel.py
+++ python/nearust/_symdel.py
@@ -172,6 +172,9 @@ class CachedSymdel:
         >>> cached_maxd2 = nearust.CachedSymdel(["fooo", "barr", "bazz", "buzz"], max_distance=2)
         >>> cached_maxd2.symdel(["fizz", "fuzz", "buzz"], max_distance=2)
         [(0, 2, 2), (0, 3, 2), (1, 2, 2), (1, 3, 1), (2, 2, 1), (2, 3, 0)]
+        >>> cached_maxd2.symdel(["fizz", "fuzz", "buzz"])
+        [(1, 3, 1), (2, 2, 1), (2, 3, 0)]
+        >>> # max_distance > 2 will throw an error!: cached_maxd2.symdel(["fizz", "fuzz", "buzz"], max_distance=3)
         """
         if query is None:
             return self._internal.symdel_within(max_distance, zero_index)
