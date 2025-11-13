af3522bb8df6daede19ea7480eadd66db27025ae
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-11-13 21:01:04 +0000

    fix(profiling): use 1m files for memray profiling

--- profiling/scripts/profiling_cross.py
+++ profiling/scripts/profiling_cross.py
@@ -2,6 +2,6 @@ import nearust
 import pandas as pd
 
 if __name__ == "__main__":
-    query = pd.read_csv("./test_files/cdr3b_10k_a.txt", header=None)
-    ref = pd.read_csv("./test_files/cdr3b_10k_b.txt", header=None)
+    query = pd.read_csv("./test_files/cdr3b_1m_a.txt", header=None)
+    ref = pd.read_csv("./test_files/cdr3b_1m_b.txt", header=None)
     _ = nearust.symdel(query[0], ref[0])
--- profiling/scripts/profiling_memoized.py
+++ profiling/scripts/profiling_memoized.py
@@ -2,7 +2,7 @@ import nearust
 import pandas as pd
 
 if __name__ == "__main__":
-    query = pd.read_csv("./test_files/cdr3b_10k_a.txt", header=None)
-    ref = pd.read_csv("./test_files/cdr3b_10k_b.txt", header=None)
+    query = pd.read_csv("./test_files/cdr3b_1m_a.txt", header=None)
+    ref = pd.read_csv("./test_files/cdr3b_1m_b.txt", header=None)
     cached = nearust.CachedSymdel(ref[0])
     _ = cached.symdel(query[0])
