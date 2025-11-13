15c1d2a8b7ea83ca6e2eb5bbf05f7bffd9d71efc
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-11-13 23:37:01 +0000

    fix(profiling): improve profiling output file names

--- run_profiling.sh
+++ run_profiling.sh
@@ -16,14 +16,14 @@ if [ ! -d "profiling/results" ]; then
 fi
 
 echo "Running perf on CLI..."
-perf record -g -o "profiling/results/${CURRENT_COMMIT}_perf_within_${CURRENT_TIME}.data" ./target/release/nearust test_files/cdr3b_1m_a.txt > /dev/null
-perf record -g -o "profiling/results/${CURRENT_COMMIT}_perf_cross_${CURRENT_TIME}.data" ./target/release/nearust test_files/cdr3b_1m_a.txt test_files/cdr3b_1m_b.txt > /dev/null
+perf record -g -o "profiling/results/perf.data.within.${CURRENT_COMMIT}" ./target/release/nearust test_files/cdr3b_1m_a.txt > /dev/null
+perf record -g -o "profiling/results/perf.data.cross.${CURRENT_COMMIT}" ./target/release/nearust test_files/cdr3b_1m_a.txt test_files/cdr3b_1m_b.txt > /dev/null
 
 echo "Running memray on Python package..."
 memray run -o "/tmp/memray_within_${CURRENT_TIME}.bin" --aggregate ./profiling/scripts/profiling_within.py
 memray run -o "/tmp/memray_cross_${CURRENT_TIME}.bin" --aggregate ./profiling/scripts/profiling_cross.py
 memray run -o "/tmp/memray_memoized_${CURRENT_TIME}.bin" --aggregate ./profiling/scripts/profiling_memoized.py
 
-memray flamegraph -o "profiling/results/${CURRENT_COMMIT}_memray_within_${CURRENT_TIME}.html" "/tmp/memray_within_${CURRENT_TIME}.bin"
-memray flamegraph -o "profiling/results/${CURRENT_COMMIT}_memray_cross_${CURRENT_TIME}.html" "/tmp/memray_cross_${CURRENT_TIME}.bin"
-memray flamegraph -o "profiling/results/${CURRENT_COMMIT}_memray_memoized_${CURRENT_TIME}.html" "/tmp/memray_memoized_${CURRENT_TIME}.bin"
+memray flamegraph -o "profiling/results/memray.within.${CURRENT_COMMIT}.html" "/tmp/memray_within_${CURRENT_TIME}.bin"
+memray flamegraph -o "profiling/results/memray.cross.${CURRENT_COMMIT}.html" "/tmp/memray_cross_${CURRENT_TIME}.bin"
+memray flamegraph -o "profiling/results/memray.memoized.${CURRENT_COMMIT}.html" "/tmp/memray_memoized_${CURRENT_TIME}.bin"
