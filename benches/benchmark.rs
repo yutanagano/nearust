15260bae7cee98266c9b5b37b6db1f54e20c5b26
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-11-22 16:48:26 +0000

    wip(bench): further set up benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,18 +1,27 @@
-use _lib::{get_candidates_within, get_input_lines_as_ascii};
+use _lib::{get_candidates_cross, get_candidates_within, get_input_lines_as_ascii};
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::fs::File;
 use std::io::BufReader;
 
-pub fn within_bench(c: &mut Criterion) {
+fn setup_benchmarks(c: &mut Criterion) {
     let f = BufReader::new(File::open("test_files/cdr3b_10k_a.txt").unwrap());
-    let test_input = get_input_lines_as_ascii(f).expect("can't read test input file");
+    let query = get_input_lines_as_ascii(f).expect("can't read test query");
+
+    let f = BufReader::new(File::open("test_files/cdr3b_10k_b.txt").unwrap());
+    let reference = get_input_lines_as_ascii(f).expect("can't read test reference");
 
     c.bench_function("within", |b| {
         b.iter(|| {
-            let _ = get_candidates_within(&test_input, 1);
+            let _ = get_candidates_within(&query, 1);
+        })
+    });
+
+    c.bench_function("cross", |b| {
+        b.iter(|| {
+            let _ = get_candidates_cross(&query, &reference, 1);
         })
     });
 }
 
-criterion_group!(bench, within_bench);
+criterion_group!(bench, setup_benchmarks);
 criterion_main!(bench);
