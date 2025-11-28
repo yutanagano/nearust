a830149386b28a59e80d162e36d6b938bde0fe73
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-11-28 22:56:01 +0000

    chore(profiling): remove scripts now unnecessary

--- profiling/scripts/get_version.py
+++
@@ -1,4 +0,0 @@
-from importlib import metadata
-
-if __name__ == "__main__":
-    print(metadata.version("nearust"))
--- profiling/scripts/profiling_within.py
+++
@@ -1,6 +0,0 @@
-import nearust
-import pandas as pd
-
-if __name__ == "__main__":
-    df = pd.read_csv("./test_files/cdr3b_1m_a.txt", header=None)
-    _ = nearust.symdel(df[0])
