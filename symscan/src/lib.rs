417e8c7ba9deabadfc94a4dafbd92185dd18f684
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-12-11 19:15:32 +0000

    fix(docs): link to initial symmetric deletion blog post

--- symscan/src/lib.rs
+++ symscan/src/lib.rs
@@ -1,15 +1,17 @@
 //! SymScan enables extremely fast discovery of pairs of similar strings within and across large
 //! collections.
 //!
-//! SymScan is a variation on the [SymSpell](https://github.com/wolfgarbe/SymSpell) algorithm that
-//! is optimised for bulk-searching similar strings within one or across two large string
-//! collections at once (e.g. searching for similar protein sequences among a collection of 10M).
-//! The key algorithmic difference between SymScan and traditional SymSpell is the use of a
-//! [sort-merge join](https://en.wikipedia.org/wiki/Sort-merge_join) approach in place of hashmaps
-//! to discover input strings that share common deletion variants. This sort-and-scan approach
-//! trades off an additional factor of O(log N) in expected time complexity for improved cache
-//! locality and effective parallelization, and ends up being much faster for the above use case.
-//! See [`get_neighbors_within`] and [`get_neighbors_across`] for details on the API.
+//! SymScan is a variation on the [symmetric deletion
+//! ](https://seekstorm.com/blog/1000x-spelling-correction/) algorithm that is optimised for
+//! bulk-searching similar strings within one or across two large string collections at once (e.g.
+//! searching for similar protein sequences among a collection of 10M). The key algorithmic
+//! difference between SymScan and traditional symmetric deletion is the use of a [sort-merge
+//! join](https://en.wikipedia.org/wiki/Sort-merge_join) approach in place of hashmaps to discover
+//! input strings that share common deletion variants. This sort-and-scan approach trades off an
+//! additional factor of O(log N) (with N the total number of strings being compared) in expected
+//! time complexity for improved cache locality and effective parallelization, and ends up being
+//! much faster for the above use case. See [`get_neighbors_within`] and [`get_neighbors_across`]
+//! for details on the API.
 //!
 //! Even for our intended use case of discovering pairs of similar strings from large collections,
 //! it is sometimes useful to memoize the deletion variant computations for at least one side of
